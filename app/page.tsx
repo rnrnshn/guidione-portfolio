a181a2ed618ab157ecb8091c012bbdf0253a3153
rnrnshn <olymangue@gmail.com>
2024-08-22 13:31:43 +0300

    chore: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,152 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="min-h-screen max-w-screen-2xl mx-auto flex flex-col">
+      <header className="flex justify-between items-center sm:px-4 sm:py-2">
+        <div>
+          <Image
+            src='/logo.png'
+            alt="Logo"
+            width={50}
+            height={50}
+            className="rounded-full"
+          />
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <nav>
+          <Link href='/'>Projects</Link>
+          <Link href='/'>About</Link>
+          <Link href='/'>Contact</Link>
+        </nav>
+      </header>
+      <main className="sm:pb-8 sm:px-4">
+        <section className="sm:pl-80 sm:py-14 sm:pr-72">
+          <h1 className="text-4xl font-extralight leading-relaxed">
+            Hello, I'm <span>Guidione</span>. <br />
+            I am <span>France</span> based Product Designer. <br />
+            I lead design at <span>23point5</span> previously at <span>Shopify</span>.
+          </h1>
+        </section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <section className="">
+          <div className="grid grid-cols-3 gap-4">
+            <aside
+              className="relative flex flex-col aspect-custom bg-slate-500 rounded-3xl"
+            >
+              <div className="absolute bottom-0 left-0 z-10 sm:p-4">
+                <h2 className="text-sm">
+                  23point5
+                </h2>
+                <h3 className="text-sm">
+                  An innovative apparel technology and manufacturing platform
+                </h3>
+              </div>
+              <video
+                preload="auto"
+                loop
+                autoPlay
+                className="absolute inset-0 w-full h-full object-cover object-center rounded-3xl"
+              >
+                <source src="/23point5-video.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </aside>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            <aside
+              className="relative flex flex-col bg-slate-500 aspect-custom rounded-3xl"
+            >
+              <div className="absolute bottom-0 left-0 z-10 sm:p-4">
+                <h2 className="text-sm">
+                  Shopify Blockchain
+                </h2>
+                <h3 className="text-sm">
+                  Building for blockchain/token-powered commerce
+                </h3>
+              </div>
+              <video
+                preload="auto"
+                loop
+                autoPlay
+                className="absolute inset-0 w-full h-full object-cover object-center rounded-3xl"
+              >
+                <source src="/23point5-video.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </aside>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            <aside
+              className="relative flex flex-col bg-slate-500 rounded-3xl"
+            >
+              <div className="absolute bottom-0 left-0 z-10 sm:p-4">
+                <h2 className="text-sm">
+                  Shopify - NFT Sales Eligibility Application
+                </h2>
+                <h3 className="text-sm">
+                  An application helps merchants in selling, gifting, and minting NFTs.
+                </h3>
+              </div>
+              <video
+                preload="auto"
+                loop
+                autoPlay
+                className="absolute inset-0 w-full h-full object-cover object-center rounded-3xl"
+              >
+                <source src="/23point5-video.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </aside>
+          </div>
+        </section>
+      </main>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <footer className="sm:px-48 sm:pb-16 sm:pt-24">
+        <div className="flex flex-col gap-2">
+          <h3 className="sm:text-xl font-light">Get in touch</h3>
+
+          <div className="flex justify-between">
+            <div className="flex gap-8">
+              <aside className="flex flex-col gap-2">
+                <h4 className="text-xs font-light">Email</h4>
+                <Link
+                  href='mailto:guidione.machava@gmail.com'
+                  className="font-light"
+                >
+                  guidione.machava@gmail.com
+                </Link>
+              </aside>
+
+              <aside className="flex flex-col gap-2">
+                <h4 className="text-xs font-light">LinkedIn</h4>
+                <Link
+                  href='https://www.linkedin.com/in/guidionemachava/'
+                  className="font-light"
+                >
+                  Guidione Machava
+                </Link>
+              </aside>
+            </div>
+
+            <div className="flex gap-8">
+              <Link
+                href='https://drive.google.com/uc?export=download&id=1x-ly9V_TByeLn2BPBBep3852qxrNd2tr'
+                className="font-light flex items-center justify-center sm:px-4 sm:py-2 border border-slate-900 rounded-full"
+              >
+                <span>Resume</span>
+              </Link>
+
+              <Link
+                href="https://drive.google.com/uc?export=download&id=1vUawRiuFzsOh2shYTryyutMDnse2Vt6p"
+                className="font-light flex items-center justify-center sm:px-4 sm:py-2 border border-slate-900 rounded-full"
+              >
+                <span>Portfolio</span>
+              </Link>
+            </div>
+
+          </div>
+        </div>
+      </footer >
+    </div >
   );
 }
