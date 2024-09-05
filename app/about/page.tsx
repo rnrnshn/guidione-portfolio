b113ddde6a9e33a51f943c8d9419148f25acd773
rnrnshn <olymangue@gmail.com>
2024-09-05 10:18:26 +0300

    feat: add education component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ export default function About() {
   return (
     <div className="max-w-[1880px] mx-auto">
       <Header />
-      <main className="px-4 sm:px-4">
+      <main className="px-4 sm:px-4 sm:mb-20">
         <Image
           src={GuidioneWide}
           alt="Logo"
@@ -21,7 +21,7 @@ export default function About() {
             I am a Product Designer with +9 years of working experience. I have worked for Shopify - Blockchain team, The World Bank, and a few early-stage startups. I hold a degree in Economics and a not concluded Honours in Interaction Design. I am a passionate design Advocate, I have authored a book, spearheaded conferences, and spoken at multiple events worldwide
           </p>
 
-          <div className="justify-center bg-gray-100">
+          <div className="justify-center bg-gray-100 mb-16">
             <Accordion>
               { /* @ts-ignore */}
               <AccordionItem title="23point5" subTitle="Lead Product Designer">
@@ -60,6 +60,59 @@ export default function About() {
                 </p>
               </AccordionItem>
             </Accordion>
+          </div>
+
+          <div className="flex gap-32">
+            <aside>
+              <h3 className="pb-4 mb-8 text-3xl font-serif italic text-zinc-600 border-b border-gray-200">Education</h3>
+              <div className="mb-8 leading-relaxed">
+                <h4 className="font-semibold">The Open Window Institute</h4>
+                <p>Honors in Interaction Design</p>
+                <p>2019 - 2021</p>
+                <p>South Africa</p>
+              </div>
+
+              <div className="leading-relaxed">
+                <h4 className="font-semibold">St. Thomas University</h4>
+                <p>Bachelor's in Economicsn</p>
+                <p>2012 - 2015 </p>
+                <p>Mozambique</p>
+              </div>
+            </aside>
+
+            <aside>
+              <h3 className="pb-4 mb-8 text-3xl font-serif italic text-zinc-600 border-b border-gray-200">Volunteering</h3>
+              <div className="mb-8 leading-relaxed">
+                <h4 className="font-semibold">IxDA Global</h4>
+                <p>Support Team Interaction 20</p>
+                <p>Feb - 2020 Milan, Italy </p>
+                <p>Milan, Italy</p>
+              </div>
+
+              <div className="leading-relaxed">
+                <h4 className="font-semibold">TEDx Pretoria</h4>
+                <p>Support Team </p>
+                <p>Nov 2018</p>
+                <p>Pretoria, South Africa</p>
+              </div>
+            </aside>
+
+            <aside>
+              <h3 className="pb-4 mb-8 text-3xl font-serif italic text-zinc-600 border-b border-gray-200">Personal projects</h3>
+              <div className="mb-8 leading-relaxed">
+                <h4 className="font-semibold">World-class Designer</h4>
+                <p>Online design school for early-stage designers.</p>
+                <p>Feb - 2020, present </p>
+                <p>Pretoria, South Africa</p>
+              </div>
+
+              <div className="leading-relaxed">
+                <h4 className="font-semibold">Design Sutra (book)</h4>
+                <p>Collection of 40 timeless design principles</p>
+                <p>August - 2020</p>
+                <p>Maputo, Mozambique</p>
+              </div>
+            </aside>
           </div>
         </section>
 
