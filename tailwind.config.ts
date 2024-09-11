8678246de7dc77c4c1e7e87419a7955f1f238c9d
rnrnshn <olymangue@gmail.com>
2024-09-11 10:05:29 +0300

    style(tailwind.config.js): add image section and container aspect ratio

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,9 @@ const config: Config = {
     extend: {
       aspectRatio: {
         'custom': '0.765101 / 1',
-        'project-cover': '1.94444 / 1'
+        'project-cover': '1.94444 / 1',
+        'image-section': '1.75 / 1',
+        'image-container': '3.5 / 1'
       },
       fontFamily: {
         sans: ['var(--font-inter_tight)'],
