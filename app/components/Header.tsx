cd5e70fb873a7d201dc4f0e661f0d16db4adb46c
rnrnshn <olymangue@gmail.com>
2024-09-25 07:34:24 +0200

    feat(Header): update header with active and hover states

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,15 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export function Header() {
+  const pathname = usePathname();
+
+  const linkClasses = "hover:text-orange-500 text-gray-700 transition-all duration-300 ease-in-out";
+  const activeLinkClasses = "border-b-2 border-orange-500 text-orange-500 mb-0 pb-1"; // Styles for active link
+
   return (
     <header className="flex justify-between items-center px-4 pt-4 sm:px-4 sm:py-2">
       <div>
@@ -15,10 +23,16 @@ export function Header() {
       </div>
 
       <nav className="flex gap-4">
-        <Link href='/'>Projects</Link>
-        <Link href='/about'>About</Link>
-        <Link href='/'>Contact</Link>
+        <Link href='/' className={`${linkClasses} ${pathname === '/' ? activeLinkClasses : ''}`}>
+          Projects
+        </Link>
+        <Link href='/about' className={`${linkClasses} ${pathname === '/about' ? activeLinkClasses : ''}`}>
+          About
+        </Link>
+        <Link href='/contact' className={`${linkClasses} ${pathname === '/contact' ? activeLinkClasses : ''}`}>
+          Contact
+        </Link>
       </nav>
     </header>
-  )
+  );
 }
